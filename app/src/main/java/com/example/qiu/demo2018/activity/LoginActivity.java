56d5c246efc126f6e1d94c5f1c6297ea8d102301
hujian <745499691@qq.com>
2018-04-03 12:43:04 +0800

    hujian

--- app/src/main/java/com/example/qiu/demo2018/activity/LoginActivity.java
+++ app/src/main/java/com/example/qiu/demo2018/activity/LoginActivity.java
@@ -1,11 +1,8 @@
 package com.example.qiu.demo2018.activity;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.qiu.demo2018.R;
-
 public class LoginActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/example/qiu/demo2018/activity/Main2Activity.java
@@ -0,0 +1,14 @@
+package com.example.qiu.demo2018.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.qiu.demo2018.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
